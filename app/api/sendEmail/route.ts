ab68c828f13c685cea7bb13a2cc4901eec524015
ufraaan <ufraansiddiqui30@gmail.com>
2024-10-27 01:48:19 +0530

    hardcode creds for testing

--- app/api/sendEmail/route.ts
+++ app/api/sendEmail/route.ts
@@ -1,43 +1,44 @@
-
 import nodemailer from 'nodemailer';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
-    const { name, email, contactNo, semester, group, subject, resourceType, note, downloadURL } = await request.json();
+    const { name, email, contactNo, semester, group, subject, resourceType, note, downloadURL } = await request.json();
 
-    
-    const transporter = nodemailer.createTransport({
-        service: 'gmail', 
-        auth: {
-            user: process.env.EMAIL_USER, 
-            pass: process.env.EMAIL_PASS, 
-        },
-    });
+    
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'kurosen930@gmail.com', 
+            pass: 'wviupaflyroxesfw', 
+        },
+    });
 
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to: 'kurosen930@gmail.com',
-        subject: 'Archivos - New Resource Uploaded',
-        text: `
-            Name: ${name}
-            Email: ${email}
-            Contact No: ${contactNo}
-            Semester: ${semester}
-            Group: ${group}
-            Subject: ${subject}
-            Resource Type: ${resourceType}
-            Note: ${note}
-            Download URL: ${downloadURL}
-        `,
-    };
+    const mailOptions = {
+        from: 'kurosen930@gmail.com',
+        to: 'kurosen930@gmail.com',
+        subject: 'Archivos - New Resource Uploaded',
+        text: `
+            Name: ${name}
+            Email: ${email}
+            Contact No: ${contactNo}
+            Semester: ${semester}
+            Group: ${group}
+            Subject: ${subject}
+            Resource Type: ${resourceType}
+            Note: ${note}
+            Download URL: ${downloadURL}
+        `,
+    };
 
-    try {
-        await transporter.sendMail(mailOptions);
-        return NextResponse.json({ success: true });
-    } catch (error) {
-        return NextResponse.json({
-            success: false,
-            error: (error as Error).message, 
-        });
-    } 
-} 
+    try {
+        const info = await transporter.sendMail(mailOptions);
+        console.log("Email sent: ", info.response);
+        return NextResponse.json({ success: true, response: info.response });
+    } catch (error) {
+        console.error("Error sending email: ", error);
+        return NextResponse.json({
+            success: false,
+            error: (error as Error).message,
+        });
+    } 
+}

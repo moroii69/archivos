e91da6edadecf1a6a46896f2d594a60ee037629c
ufraaan <ufraansiddiqui30@gmail.com>
2024-10-27 13:01:01 +0530

    temporarily remove drag and drop method to upload

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -176,7 +176,7 @@
                     <label htmlFor="file-upload" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
                         <div className="flex flex-col items-center justify-center pt-5 pb-6">
                         <Upload className="w-8 h-8 mb-4 text-gray-500" />
-                        <p className="mb-2 text-sm text-gray-500"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                        <p className="mb-2 text-sm text-gray-500"><span className="font-semibold">Click to upload</span></p>
                         <p className="text-xs text-gray-500">PDF (MAX. 10MB)</p>
                         </div>
                         <Input id="file-upload" type="file" accept=".pdf" className="hidden" onChange={handleFileChange} />
